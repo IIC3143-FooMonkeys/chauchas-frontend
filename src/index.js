84d24f8afec0d14c466e1c6210388cdace598ce0
Martín Araya <mbozo@uc.cl>
2024-05-16 19:01:36 -0400

    Cloudfront te odio

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
-console.log("Auth0 Domain:", domain)
-console.log("Auth0 Client ID:", clientId)
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
